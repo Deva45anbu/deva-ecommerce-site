ed1b81760ff5a50077402f6c5c74bd016262b7d8
Deva45 <devaanbu45@gmail.com>
2024-05-23 16:16:00 +0530

    removed unused codes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter} from 'react-router-dom';
 import './App.css';
-import { Home } from './Components/Home';
-import { Login } from './Components/login';
 import { RouteData } from './Route';
 
 function App() {
